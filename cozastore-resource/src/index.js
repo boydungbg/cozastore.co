43d007f47fccedd61ff03d6d9f6ce721692441e1
nssoldier <vuongphongkcs@gmail.com>
2020-08-18 00:51:57 +0700

    ok

--- cozastore-resource/src/index.js
+++ cozastore-resource/src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./vendor/bootstrap/css/bootstrap.min.css";
-import "./fonts/font-awesome-4.7.0/css/font-awesome.min.css";
-import "./fonts/iconic/css/material-design-iconic-font.min.css";
-import "./fonts/linearicons-v1.0.0/icon-font.min.css";
-import "./vendor/animate/animate.css";
-import "./vendor/css-hamburgers/hamburgers.min.css";
-import "./vendor/animsition/css/animsition.min.css";
-import "./vendor/select2/select2.min.css";
-import "./vendor/daterangepicker/daterangepicker.css";
-import "./vendor/slick/slick.css";
-import "./vendor/MagnificPopup/magnific-popup.css";
-import "./css/util.css";
-import "./css/main.css";
-import "./vendor/fontawesome-free/css/all.min.css";
-import "./css/sb-admin-2.min.css"
+// import "./vendor/bootstrap/css/bootstrap.min.css";
+// import "./fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+// import "./fonts/iconic/css/material-design-iconic-font.min.css";
+// import "./fonts/linearicons-v1.0.0/icon-font.min.css";
+// import "./vendor/animate/animate.css";
+// import "./vendor/css-hamburgers/hamburgers.min.css";
+// import "./vendor/animsition/css/animsition.min.css";
+// import "./vendor/select2/select2.min.css";
+// import "./vendor/daterangepicker/daterangepicker.css";
+// import "./vendor/slick/slick.css";
+// import "./vendor/MagnificPopup/magnific-popup.css";
+// import "./css/util.css";
+// import "./css/main.css";
+// import "./vendor/fontawesome-free/css/all.min.css";
+// import "./css/sb-admin-2.min.css"
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
